db1482123ff80bbd1ee328a1bef0638761873bf7
pramit2012 <101552755+pramit2012@users.noreply.github.com>
2023-02-06 19:47:59 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+status = "";
+function setup() {
+    canvas = createCanvas(380, 380);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(380, 380);
+    video.hide();
+}
+function start() {
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+    document.getElementById("name_of_object").value;
+}
+function modelLoaded() {
+    console.log("Model Loaded!");
+    status = true;
+}
+function draw() {
+    image(video, 0, 0, 380, 380);
+}
